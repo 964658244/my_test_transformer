1960f39ffe103e947f9ebf515b2d6eb14a47ee2b
lixinxing <964658244@qq.com>
2024-09-27 08:11:07 +0800

    add decoder

--- src/models/decoder.py
+++ src/models/decoder.py
@@ -10,3 +10,24 @@ class DecoderLayer(nn.Module):
         self.mha = nn.ModuleList([MultiHeadAttention(h_head, d_model, dropout) for _ in range(2)])
         # define a feedforward network layer
         self.ffn = PositionWiseFFN(d_model, dropout)
+
+    def forward(self, yz, xz, yz_look_ahead_mask, xz_pad_mask):
+        # execute the first attention layer calculation, using self attention
+        dec_output = self.mha[0](yz, yz, yz, yz_look_ahead_mask)
+        # execute the second attention layer calculation, Q from previous attention layer, K and V from encoder output
+        dec_output = self.mha[1](dec_output, xz, xz, xz_pad_mask)
+        dec_output = self.ffn(dec_output)
+        return dec_output
+
+
+class Decoder(nn.Module):
+    def __init__(self, n_head, d_model, dropout, n_layer):
+        super().__init__()
+        # define n_layer decoderLayer restored in ModuleList
+        self.num_layers = n_layer
+        self.decoder_layers = nn.ModuleList([DecoderLayer(n_head, d_model, dropout) for _ in range(n_layer)])
+
+    def forward(self, yz, xz, yz_look_ahead_mask, xz_pad_mask):
+        for decoder in self.decoder_layers:
+            yz = decoder(yz, xz, yz_look_ahead_mask, xz_pad_mask)
+        return yz
